06aeac8ef96339fd101ea36c7517201626ee9f46
Alice Rovere <arovere1@gmail.com>
2026-03-17 16:48:15 +0100

    tolto script, rallentavano

--- script2.js
+++
@@ -1,5 +0,0 @@
-// Script per pagina2.html
-const opere = [
-    // Inserisci qui i pannelli specifici di pagina 2
-];
-// ...Copia qui la logica JS necessaria solo per pagina2...
--- script3.js
+++
@@ -1,5 +0,0 @@
-// Script per pagina3.html
-const opere = [
-    // Inserisci qui i pannelli specifici di pagina 3
-];
-// ...Copia qui la logica JS necessaria solo per pagina3...
